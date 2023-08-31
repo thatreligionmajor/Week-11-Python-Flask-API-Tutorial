87b18be5dc5e82640f64b3b2ad2c2488d49fb9d6
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-08-31 00:53:33 +0000

    I cannot get Learnpack to accept my tests, but I am fairly certain this is correct.

--- src/app.py
+++ src/app.py
@@ -1,25 +1,29 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 app = Flask(__name__)
+from flask import request
 
-todos = [ { "label": "My first task", "done": False } ]
+todos = [
+    {"label": "My first task", "done": False},
+    {"label": "My second task", "done": False}
+]
+
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
-    return jsonify(todos)
+def hello_world(): 
+    json_text = jsonify(todos)
+    return json_text
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
-    todos.append(request_body)
-    return jsonify(todos)
+    return 'Response for the POST todo'
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete: ", position)
+    todos.pop((position-1))
     return jsonify(todos)
 
-
-# These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
